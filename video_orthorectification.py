1420075453cd01606c54f0af4fb87d7c4fde9621
jowe9773 <160812661+jowe9773@users.noreply.github.com>
2024-03-10 13:57:11 -0600

    commenting and cleaning code

--- video_orthorectification.py
+++ video_orthorectification.py
@@ -5,7 +5,6 @@ steps to orthrectify videos. Once it is done, it will be
 broken up in the way that makes the most sense."""
 
 #Load neccesary packages and modules
-import csv
 import os
 import numpy as np
 import cv2
@@ -33,42 +32,22 @@ src_pts = np.float32(src_pts[:, np.newaxis, :])
 dst_pts = np.array(gcps_rw_list)
 dst_pts = np.float32(dst_pts[:, np.newaxis, :])
 
-#####################################
-# Now we can find homography matrix #
-#####################################
-
+# Now we can find homography matrix 
 h_matrix = cv2.findHomography(src_pts, dst_pts)
 
-#Step 2: load image file
-image_fn = file_manager.load_fn("Select and image to orthorectify")
-out_image_fn = os.path.splitext(image_fn)[0] + "_warped.jpg"
-image = cv2.imread(image_fn)
+#################################################
+# Step 2: load image and apply perspective warp #
+#################################################
 
-warped_image = cv2.warpPerspective(image, h_matrix[0], (2438, 4000))
+#load image
+image_fn = file_managers.load_fn("Select and image to orthorectify")
+image = cv2.imread(image_fn) 
 
-# Save the warped image
-cv2.imwrite(out_image_fn, warped_image)
+#create an output name in the same directory as the input
+out_image_fn = os.path.splitext(image_fn)[0] + "corrected.jpg"
 
+#finally warp the perspective of the image
+corrected_image = cv2.warpPerspective(image, h_matrix[0], (2438, 4000))
 
-####################################
-# Concatenate the 4 images into 1: #
-####################################
-
-'''cam1 = file_manager.load_fn("Cam1")
-cam2 = file_manager.load_fn("Cam2")
-cam3 = file_manager.load_fn("Cam3")
-cam4 = file_manager.load_fn("Cam4")
-
-im1 = cv2.imread(cam1)
-im2 = cv2.imread(cam2)
-im3 = cv2.imread(cam3)
-im4 = cv2.imread(cam4)
-
-# vertically concatenates images
-# of same width
-im_h = cv2.hconcat([im1, im2, im3, im4])
-
-# save the output image
-out_dn = file_manager.load_dn("select directory to store image in")
-out_path = out_dn + "//merged.jpg"
-cv2.imwrite(out_path, im_h)'''
+# Save the corrected image
+cv2.imwrite(out_image_fn, corrected_image)
