f57ffdf97131527ec17e433d1e834f7f6a0d918e
jowe9773 <160812661+jowe9773@users.noreply.github.com>
2024-03-11 15:08:01 -0600

    Change video codec to hevc

--- video_orthorectification.py
+++ video_orthorectification.py
@@ -35,7 +35,7 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 os.environ['OPENCV_FFMPEG_READ_ATTEMPTS'] = '10000'  # update ffmpeg read attempts
-fourcc = cv2.VideoWriter_fourcc(*"fdsc")
+fourcc = cv2.VideoWriter_fourcc(*"hevc")
 # Create VideoWriter object to save the output video
 out = cv2.VideoWriter(output_path, fourcc, fps, (2438, 4000))
 print('done!')
