013858c0a23d52610e30ae74166a261c937ba457
jowe9773 <160812661+jowe9773@users.noreply.github.com>
2024-03-17 16:08:01 -0600

    I was running this code to make some videos. Works.

--- merge_videos.py
+++ merge_videos.py
@@ -7,7 +7,7 @@ import cv2
 from file_managers import FileManagers
 from orthomosaic_tools import OrthomosaicTools
 
-def orthorectify_video(vid, vid1, vid2, vid3, output_dn, gcps, gcps1, gcps2, gcps3, start_time_s, length_s, final_shape = (2438,4000)):
+def orthorectify_video(vid, vid1, vid2, vid3, output_dn, outname, gcps, gcps1, gcps2, gcps3, start_time_s, length_s, final_shape = (2438,4000)):
     """ method for orthorecifying videos"""
 
     #instantiate the orthomosaic tools module
@@ -20,7 +20,7 @@ def orthorectify_video(vid, vid1, vid2, vid3, output_dn, gcps, gcps1, gcps2, gcp
     matrix3 = ot.find_homography(4, gcps3)
 
     #choose a place to store output video
-    output_fn = output_dn + "\\merged.mp4"
+    output_fn = output_dn + "\\" + outname + ".mp4"
 
     #heres where we get into the video
     cap = cv2.VideoCapture(vid)
@@ -32,7 +32,7 @@ def orthorectify_video(vid, vid1, vid2, vid3, output_dn, gcps, gcps1, gcps2, gcp
 
     fourcc = cv2.VideoWriter_fourcc(*"HEIC")
     # Create VideoWriter object to save the output video
-    out = cv2.VideoWriter(output_fn, fourcc, fps, (4876, 2000))
+    out = cv2.VideoWriter(output_fn, fourcc, fps*4, (1948, 800))
     print('done!')
 
     start_time = start_time_s *1000
@@ -44,21 +44,21 @@ def orthorectify_video(vid, vid1, vid2, vid3, output_dn, gcps, gcps1, gcps2, gcp
     ret3, frame3 = cap3.read()
 
     cap.set(cv2.CAP_PROP_POS_MSEC,(start_time))
-    cap1.set(cv2.CAP_PROP_POS_MSEC,(start_time))
-    cap2.set(cv2.CAP_PROP_POS_MSEC,(start_time))
-    #cap3.set(cv2.CAP_PROP_POS_MSEC,(start_time))
+    cap1.set(cv2.CAP_PROP_POS_MSEC,(start_time + 117000))
+    cap2.set(cv2.CAP_PROP_POS_MSEC,(start_time + 114500))
+    cap3.set(cv2.CAP_PROP_POS_MSEC,(start_time + 113600))
 
     while ret and ret1 and ret2 and ret3 and count <= length_s:
 
         # correct frames with warpPerspective
         corrected_frame = cv2.warpPerspective(frame, matrix, final_shape)
-        corrected_frame = cv2.resize(corrected_frame, (1219,2000))
+        corrected_frame = cv2.resize(corrected_frame, (487, 800))
         corrected_frame1 = cv2.warpPerspective(frame1, matrix1, final_shape)
-        corrected_frame1 = cv2.resize(corrected_frame1, (1219,2000))
+        corrected_frame1 = cv2.resize(corrected_frame1, (487, 800))
         corrected_frame2 = cv2.warpPerspective(frame2, matrix2, final_shape)
-        corrected_frame2 = cv2.resize(corrected_frame2, (1219,2000))
+        corrected_frame2 = cv2.resize(corrected_frame2, (487, 800))
         corrected_frame3 = cv2.warpPerspective(frame3, matrix3, final_shape)
-        corrected_frame3 = cv2.resize(corrected_frame3, (1219,2000))
+        corrected_frame3 = cv2.resize(corrected_frame3, (487, 800))
 
         merged = cv2.hconcat([corrected_frame, corrected_frame1, corrected_frame2, corrected_frame3])
         out.write(merged)
@@ -83,11 +83,17 @@ def orthorectify_video(vid, vid1, vid2, vid3, output_dn, gcps, gcps1, gcps2, gcp
 fm = FileManagers()
 
 #select videos
-video = fm.load_fn("Video")
-video1 = fm.load_fn("Video1")
-video2 = fm.load_fn("Video2")
-video3 = fm.load_fn("Video3")
+#video = fm.load_fn("Video")
+video = 'C:/Users/josie/OneDrive/Desktop/GoPro Video Test/01082024/20240108_Cam1.MP4'
 
+#video1 = fm.load_fn("Video1")
+video1 = 'C:/Users/josie/OneDrive/Desktop/GoPro Video Test/01082024/20240108_Cam2.MP4'
+
+#video2 = fm.load_fn("Video2")
+video2 = 'C:/Users/josie/OneDrive/Desktop/GoPro Video Test/01082024/20240108_Cam3.MP4'
+
+#video3 = fm.load_fn("Video3")
+video3 = 'C:/Users/josie/OneDrive/Desktop/GoPro Video Test/01082024/20240108_Cam4.MP4'
 
 videos = [video, video1, video2]
 print(videos)
@@ -102,6 +108,10 @@ gcps3 = fm.import_gcps()
 gcps_list = [gcps, gcps1, gcps2]
 print(gcps_list)
 
-output_path = fm.load_dn("Choose a directory to store video in")
+#output_path = fm.load_dn("Choose a directory to store video in")
 
-orthorectify_video(video, video1, video2, video3, output_path, gcps, gcps1, gcps2, gcps3, start_time_s=50, length_s=3)
+output_path = 'C:/Users/josie/OneDrive/Desktop/GoPro Video Test/01082024/'
+outn = "20240108_01_merged_long"
+
+
+orthorectify_video(video, video1, video2, video3, output_path, outn, gcps, gcps1, gcps2, gcps3, start_time_s = 2, length_s = 480)
